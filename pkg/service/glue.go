eb6e1ec79ed4bbd91f438b1f8d154e26cbd1db70
Edwin Plauchu <j4nusx@yahoo.com>
2023-09-11 10:35:03 -0500

    refactor

--- pkg/service/glue.go
+++ pkg/service/glue.go
@@ -21,6 +21,20 @@ func init() {
 	envconfig.Process("rsapi", &apiSettings)
 }
 
+func configureRouter(cc *ve.ParkingLot) *mux.Router {
+
+	router := mux.NewRouter()
+	v1 := router.PathPrefix("/v1").Subrouter()
+	cruds := v1.PathPrefix("/cruds").Subrouter()
+
+	mgmt := cruds.PathPrefix("/cars").Subrouter()
+	mgmt.HandleFunc("/list", co.ListCars(cc.DisplayAll)).Methods("GET")
+	mgmt.HandleFunc(fmt.Sprintf("/{car_id:%s}", carIDMask), co.ListCar(cc.Display)).Methods("GET")
+	mgmt.HandleFunc(fmt.Sprintf("/{car_id:%s}/delete", carIDMask), co.DeleteCar(cc.Destroy)).Methods("DELETE")
+	mgmt.HandleFunc("/create", co.CreateCar(cc.Place)).Methods("POST")
+	return router
+}
+
 // Engages the RESTful API
 func Engage(logger *logrus.Logger) (merr error) {
 
@@ -31,27 +45,16 @@ func Engage(logger *logrus.Logger) (merr error) {
 		}
 	}()
 
-	cc := ve.NewParkingLot(logger)
-
 	/* The connection of both components occurs through
 	   the router glue and its adaptive functions */
 	glue := func(api *rsapi.RestAPI) *mux.Router {
 
-		router := mux.NewRouter()
-
-		v1 := router.PathPrefix("/v1").Subrouter()
-		cruds := v1.PathPrefix("/cruds").Subrouter()
-
-		mgmt := cruds.PathPrefix("/cars").Subrouter()
-		mgmt.HandleFunc("/list", co.ListCars(cc.DisplayAll)).Methods("GET")
-		mgmt.HandleFunc(fmt.Sprintf("/{car_id:%s}", carIDMask), co.ListCar(cc.Display)).Methods("GET")
-		mgmt.HandleFunc(fmt.Sprintf("/{car_id:%s}/delete", carIDMask), co.DeleteCar(cc.Destroy)).Methods("DELETE")
-		mgmt.HandleFunc("/create", co.CreateCar(cc.Place)).Methods("POST")
+		cc := ve.NewParkingLot(logger)
+		router := configureRouter(cc)
 		return router
 	}
 
 	api := rsapi.NewRestAPI(logger, &apiSettings, glue)
-
 	api.PowerOn()
 
 	return nil
--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -1,12 +1,10 @@
 package service
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	co "immortalcrab.com/parkinglot/internal/controllers"
 	ve "immortalcrab.com/parkinglot/pkg/business"
 
 	"github.com/gorilla/mux"
@@ -25,27 +23,13 @@ func checkResponseCode(t *testing.T, expected, actual int) {
 	}
 }
 
-func glue(cc *ve.ParkingLot) *mux.Router {
-
-	router := mux.NewRouter()
-	v1 := router.PathPrefix("/v1").Subrouter()
-	cruds := v1.PathPrefix("/cruds").Subrouter()
-
-	mgmt := cruds.PathPrefix("/cars").Subrouter()
-	mgmt.HandleFunc("/list", co.ListCars(cc.DisplayAll)).Methods("GET")
-	mgmt.HandleFunc(fmt.Sprintf("/{car_id:%s}", carIDMask), co.ListCar(cc.Display)).Methods("GET")
-	mgmt.HandleFunc(fmt.Sprintf("/{car_id:%s}/delete", carIDMask), co.DeleteCar(cc.Destroy)).Methods("DELETE")
-	mgmt.HandleFunc("/create", co.CreateCar(cc.Place)).Methods("POST")
-	return router
-}
-
 func TestEmptyPersitance(t *testing.T) {
 
 	logger, _ := test.NewNullLogger()
 	cc := ve.NewParkingLot(logger)
 
 	req, _ := http.NewRequest("GET", "/v1/cruds/cars/list", nil)
-	response := executeRequest(glue(cc), req)
+	response := executeRequest(configureRouter(cc), req)
 
 	checkResponseCode(t, http.StatusNotFound, response.Code)
 }
