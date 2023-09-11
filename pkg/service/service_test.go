32bf5d55a87f6b3bbe400755c941870d54f6f379
Edwin Plauchu <j4nusx@yahoo.com>
2023-09-11 13:36:38 -0500

    deletion

--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -39,14 +41,21 @@ func TestEmptyPersitance(t *testing.T) {
 	checkResponseCode(t, http.StatusNotFound, response.Code)
 }
 
-func TestCarCreation(t *testing.T) {
+func TestCarCreationAndDeletion(t *testing.T) {
 
-	logger, _ := test.NewNullLogger()
-	cc := ve.NewParkingLot(logger)
+	cc := newParkingLotWithNullLogging()
+	router := configureRouter(cc)
 
 	var jsonStr = []byte(`{ "year":2020 }`)
 	req, _ := http.NewRequest("POST", "/v1/cruds/cars/create", bytes.NewBuffer(jsonStr))
-	response := executeRequest(configureRouter(cc), req)
-
+	response := executeRequest(router, req)
 	checkResponseCode(t, http.StatusAccepted, response.Code)
+	m := struct {
+		CarID string `json:"car_id"`
+	}{}
+
+	json.Unmarshal(response.Body.Bytes(), &m)
+	req, _ = http.NewRequest("DELETE", fmt.Sprintf("/v1/cruds/cars/%s/delete", m.CarID), nil)
+	response = executeRequest(router, req)
+	checkResponseCode(t, http.StatusOK, response.Code)
 }
