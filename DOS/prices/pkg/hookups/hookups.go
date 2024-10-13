690b2cc0b3e7b50ee0e767f55cde03cd76d974be
Edwin Plauchu <pianodaemon@gmail.com>
2024-10-13 08:55:49 -0600

    Update hookups.go

--- DOS/prices/pkg/hookups/hookups.go
+++ DOS/prices/pkg/hookups/hookups.go
@@ -10,6 +10,21 @@ func NewPricesManager() *PricesManager {
 	return &PricesManager{}
 }
 
+func (self *PricesManager) DoCreatePriceList(listName, owner string) error {
+	// Pending implementation
+	return nil
+}
+
+func (self *PricesManager) DoDeleteList(listName string) error {
+	// Pending implementation
+	return nil
+}
+
+func (self *PricesManager) DoAssignTargets(listName string, targets []string) error {
+	// Pending implementation
+	return nil
+}
+
 func (self *PricesManager) DoUpdatePrice(listName, sku, unit, material, tservicio string, price float64) error {
 	// Pending implementation
 	return nil
